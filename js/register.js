638c6e05df85380e443853c7a370847d90cb0570
yaqoob-vend <muhammad.yaqoob@publicissapeint.com>
2023-01-24 16:33:34 +0500

    Update register.js

--- js/register.js
+++ js/register.js
@@ -32,7 +32,7 @@ async function getStream() {
     video.play();
     const devices = await navigator.mediaDevices.enumerateDevices();
     const videoDevices = devices.filter((item) => item.kind === "videoinput");
-    switchCamera(videoDevices[0].deviceId);
+    switchCamera(videoDevices[1].deviceId);
   } catch (error) {
     console.log(error);
   }
