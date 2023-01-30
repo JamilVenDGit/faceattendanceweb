8c8fe81f9270cfe4d6bfa9a9b794ac0bce8c8974
yaqoob-vend <muhammad.yaqoob@publicissapeint.com>
2023-01-30 11:46:07 +0500

    Update test.js

--- js/test.js
+++ js/test.js
@@ -10,9 +10,6 @@ Promise.all([
       message: "Models loaded successfully",
     });
     console.log("model loaded...");
-    getStream().catch((error) => {
-      Emitter.emit({ message: "failed tog get stream", error });
-    });
   })
   .catch((error) => {
     Emitter.emit(Events.ERROR, { error: "Errors loading models" });
@@ -21,6 +18,7 @@ Promise.all([
 const label = "yaqoob";
 const data = {};
 const input = document.querySelector("#input-photos");
+
 input.addEventListener("change", () => {
   const files = input.files;
   for (let i = 0; i < files.length; i++) {
@@ -32,5 +30,51 @@ input.addEventListener("change", () => {
       data[label] = [picURL];
     }
   }
+
   console.log("==> data", data);
+  train();
 });
+
+async function train() {
+  try {
+    if (Object.keys(data).length > 0) {
+      Emitter.emit(Events.TRAINING_START);
+      const labeledFaceDescriptors = await loadLabeledImages();
+      const faces = labeledFaceDescriptors.map((item) => item.toJSON());
+      faces && Emitter.emit(Events.DATA, { data: JSON.stringify(faces) });
+    } else {
+      Emitter.emit(Events.NOTIFICATION, {
+        notificationType: 2,
+        message: "There are no pictures taken!",
+      });
+    }
+  } catch (error) {
+    Emitter.emit(Events.ERROR, {
+      error: "Error in training faces!" + "Error: " + error.message,
+    });
+  }
+}
+
+function loadLabeledImages() {
+  return Promise.all(
+    Object.entries(data).map(async (item) => {
+      const label = item[0];
+      const descriptions = [];
+
+      for (let i = 0; i < item[1].length; i++) {
+        const img = await faceapi.fetchImage(item[1][i]);
+        const detections = await faceapi
+          .detectSingleFace(
+            img,
+            new faceapi.SsdMobilenetv1Options({ minConfidence: 0.9 })
+          )
+          .withFaceLandmarks()
+          .withFaceDescriptor();
+        if (detections) {
+          descriptions.push(detections.descriptor);
+        }
+      }
+      return new faceapi.LabeledFaceDescriptors(label, descriptions);
+    })
+  );
+}
